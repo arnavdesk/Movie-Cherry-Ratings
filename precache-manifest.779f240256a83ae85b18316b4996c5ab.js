cc18512f7bd02db1828f159ec650980e75820ec0
arnavdesk <arnavdesk@gmail.com>
2020-08-05 05:47:12 +0530

    Updates

---
+++ precache-manifest.779f240256a83ae85b18316b4996c5ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6fce888a4824b997bb4b1297687c18",
+    "url": "/Movie-Cherry/index.html"
+  },
+  {
+    "revision": "b258b6ec71711d23bc99",
+    "url": "/Movie-Cherry/static/css/main.1a854e8a.chunk.css"
+  },
+  {
+    "revision": "b634bceec47c441c7a6b",
+    "url": "/Movie-Cherry/static/js/2.99d9247e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-Cherry/static/js/2.99d9247e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b258b6ec71711d23bc99",
+    "url": "/Movie-Cherry/static/js/main.4b91b5a7.chunk.js"
+  },
+  {
+    "revision": "8675cc76f1b22efe5f2e",
+    "url": "/Movie-Cherry/static/js/runtime-main.0b4c8afc.js"
+  }
+]);
